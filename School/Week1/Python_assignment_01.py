c77bb53cbf5aa075059bff1462026aac1a0cd21c
XanderTeWinkel <xander.te.winkel@hotmail.com>
2024-09-03 11:08:25 +0200

    Basic refinement for commands and code

--- School/Week1/Python_assignment_01.py
+++ School/Week1/Python_assignment_01.py
@@ -1,36 +1,49 @@
-#First assignment for the AD_SD
+# First assignment for the AD_SD
 
-#Objective 1, Welcome!
+# Objective 1, Welcome!
 print("*** Responsible alcohol use calculator ***\n")
 
-#Objective 2, Who's this?
-name = input("What's your name? ")
-if name == "":
-    name = "Anon"
-print("Hi", name,"!")
 
-#Objective 3, Gender?
+# Objective 2, Who's this?
+name = input("What's your name? ")
+
+# Error handling
+if name == "":  # noqa: PLC1901
+    name = "Anon"
+
+print("Hi", name, "!\n")
+
+
+# Objective 3, Gender?
 gender = input("Whats your gender? [Male/Female] ").lower()
+
 if gender == "male":
-    print("Men are awesome!")
+    print("Men are strong!\n")
     drinking_norm = 21
+
 elif gender == "female":
     drinking_norm = 14
-    print("Women are awesome!")
+    print("Women are smart!\n")
+
+# Error handling
 else:
     gender = "None"
-    print("You are special")
+    print("You are one of a kind\n")
 
-#Objective 4, Drinks?
+
+# Objective 4, Drinks?
 drinks = int(input("How many alcoholic beverage do you drink on average per week? "))
+
 try:
-    print("That's", drinks*52, "per year!")
+    print("That's", drinks*52, "per year!\n")
+
+# Error handling
 except ValueError:
     print("Invalid number error\nExiting code...")
     exit()
 
 
-#Objective 5, A word of advice
+# Objective 5, A word of advice
 # if int(drinks) <= 0:
 #     print("Healthy choice,", name)
 # elif int(drinks) <= 7:
@@ -38,27 +51,30 @@ except ValueError:
 # else:
 #     print("Drinking less would be a healthier choice,", name)
 
-#Objective 6, Gender-specific advice
+# Objective 6, Gender-specific advice
 if drinks <= 0:
     print("Healthy choice,", name)
+
 elif drinks <= 7:
     print("You'll probably be okay,", name)
-elif drinks <= drinking_norm:
+
+elif drinks <= drinking_norm: # type: ignore
     print("Drinking less would be a healthier choice,", name)
-elif drinks <= drinking_norm*2 :
-    print("You're an excessive drinker,",name,". That's not healthy..")
+
+elif drinks <= drinking_norm*2: # type: ignore
+    print("You're an excessive drinker,", name, ". That's not healthy..")
+
 else:
-    print("You're a really heavy drinker,",name,". That's very unhealthy. Change your ways!")
+    print("You're a really heavy drinker,", name, ". That's very unhealthy. Change your ways!")
 
-#Objective 7, Error handling
-#Ob 1, 
-#Ob 2, Made the user 'Anon' if they do not fill in a name.
-#Ob 3, Made a 3rd gender called 'none' with the print 'You are special'.
-#Ob 4, Made a try and except for the first 'int()' function 
-#      so if that one fails the code shuts down, Instead of throwing 
+# Objective 7, Error handling
+# Ob 1,
+# Ob 2, Made the user 'Anon' if they do not fill in a name.
+# Ob 3, Made a 3rd gender called 'none' with the print 'You are special'.
+# Ob 4, Made a try and except for the first 'int()' function
+#      so if that one fails the code shuts down, Instead of throwing
 #      more errors.
-#Ob 5, Is incorporated in Ob 6.
-#Ob 6, If gender is not male or female it doesn't give an advise.
-#      Further handling is that the input gender is set to lowercase 
+# Ob 5, Is incorporated in Ob 6.
+# Ob 6, If gender is not male or female it doesn't give an advise.
+#      Further handling is that the input gender is set to lowercase
 #      for better recognition.
-
--- School/Week1/Python_assignment_02.py
+++ School/Week1/Python_assignment_02.py
@@ -1,15 +1,15 @@
-#Second assignment for the AD_SD.
+# Second assignment for the AD_SD.
 
 print("*** Welcome to Santa's Workshop ***")
 
-#A string that if is 'yes' keeps the while loop running.
+# A string that if is 'yes' keeps the while loop running.
 keep_going = "yes"
 
-#The while loop that keeps running with all the rest of the code as long that 'keep_going' is yes.
+# The while loop that keeps running with all the rest of the code as long that 'keep_going' is yes.
 while keep_going == "yes":
     print("\nLets draw a tree!")
     
-    #A while loop that keeps asking the user for a integer from the user. If no integer is given the code keeps repeating itself.
+    # A while loop that keeps asking the user for a integer from the user. If no integer is given the code keeps repeating itself.
     while True:
         try:
             size = int(input("How tall would you like it to be? "))
@@ -17,45 +17,43 @@ while keep_going == "yes":
         except ValueError:
             print("Invalid number error")
     
-    #The if else tree for deciding what shape has to be displayed. If an incorrect shape has been given the code starts again at 
-    #'Lets draw a tree!'.
+    # The if else tree for deciding what shape has to be displayed. If an incorrect shape has been given the code starts again at 
+    # 'Lets draw a tree!'.
     shape = input("What shape should it have? [right/left/normal] ").lower()
 
-    #Code for displaying the right side tree.
+    # Code for displaying the right side tree.
     if shape == "right":
         for i in range(size):
             for j in range(i+1):
                 print("*", end="")
             print()
 
-    #Code for displaying the left side tree.
+    # Code for displaying the left side tree.
     elif shape == "left":
         for i in range(1, size + 1):
             print(" " * (size - i) + "*" * i)
 
-    #Code for displaying the normal tree.
+    # Code for displaying the normal tree.
     elif shape == "normal":
         k = 0
         for i in range(1, size+1):
             for space in range(1, (size-i)+1):
                 print(end="  ")
-            while k!=(2*i-1):
+            while k != (2*i-1):
                 print("* ", end="")
                 k += 1
             k = 0
             print()
 
-    #If an incorrect shape has been given the code starts at 'Lets draw a tree!' again.
+    # If an incorrect shape has been given the code starts at 'Lets draw a tree!' again.
     else:
         print("No such shape")
         continue
     
-    #Code for asking the user if they want to make another tree. If not 'Yes' or 'No' the code keeps asking again.
+    # Code for asking the user if they want to make another tree. If not 'Yes' or 'No' the code keeps asking again.
     while True:
         keep_going = input("Would you like to see another tree? [yes/no] ").lower()
         if keep_going == "yes" or keep_going == "no":
             break
         else:
             print("Invalid choice!")
-
-
