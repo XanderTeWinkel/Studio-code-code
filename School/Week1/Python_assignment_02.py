d3ac78030c7d71afdaf936560b7a4eda8c6ad413
XanderTeWinkel <xander.te.winkel@hotmail.com>
2024-09-03 14:50:08 +0200

    Fixing the code with the feedback from 1.1

--- School/Week1/Python_assignment_02.py
+++ School/Week1/Python_assignment_02.py
@@ -2,16 +2,15 @@
 
 print("*** Welcome to Santa's Workshop ***")
 
-# A string that if is 'yes' keeps the while loop running.
+# Function for the while loop
 keep_going = "yes"
 
 # The while loop that keeps running with all the rest of the code as long that 'keep_going' is yes.
 while keep_going == "yes":
     print("\nLets draw a tree!")
     
-    # A while loop that keeps asking the user for a integer from the user. If no integer is given the code keeps repeating itself.
-    while True:
-        try:
+    while True: # While loop for asking the height
+        try: # Try for catching an invalid height
             height = int(input("How tall would you like it to be? "))
             break
         except ValueError:
@@ -20,38 +19,32 @@ while keep_going == "yes":
     # The if else tree for deciding what shape has to be displayed. If an incorrect shape has been given the code starts again at
     # 'Lets draw a tree!'.
     shape = input("What shape should it have? [right/left/normal] ").lower()
-
-    # Code for displaying the right side tree.
-    if shape == "right":
+    
+    if shape == "right": # Code for displaying the right side tree.
         for i in range(height):
-            for _j in range(i+1):
-                print("*", end="")
+            for _j in range(i + 1):
+                print("* ", end="")
             print()
-
-    # Code for displaying the left side tree.
-    elif shape == "left":
+    elif shape == "left":     # Code for displaying the left side tree.
         for i in range(1, height + 1):
             print(" " * (height - i) + "*" * i)
-
-    # Code for displaying the normal tree.
-    elif shape == "normal":
+    elif shape == "normal":     # Code for displaying the normal tree.
         k = 0
-        for i in range(1, height+1):
-            for _space in range(1, (height-i)+1):
+        for i in range(1, height + 1):
+            for _space in range(1, (height - i) + 1):
                 print(end="  ")
-            while k != (2*i-1):
+            while k != (2 * i - 1):
                 print("* ", end="")
                 k += 1
             k = 0
             print()
-
-    # If an incorrect shape has been given the code starts at 'Lets draw a tree!' again.
     else:
+        # If an incorrect shape has been given the code starts at 'Lets draw a tree!' again.
         print("No such shape")
         continue
     
-    # Code for asking the user if they want to make another tree. If not 'Yes' or 'No' the code keeps asking again.
     while True:
+        # Code for asking the user if they want to make another tree. If not 'Yes' or 'No' the code keeps asking again.
         keep_going = input("Would you like to see another tree? [yes/no] ").lower()
         if keep_going == "yes" or keep_going == "no":
             break
